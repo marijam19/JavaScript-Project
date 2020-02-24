b29255f50c0fe2b4444924c1b4c4cc69b30e54f6
marijam19 <markovic.n.maja@gmail.com>
2020-02-24 22:30:17 +0100

    changed game

--- script.js
+++ script.js
@@ -1,196 +1,149 @@
+var fields = document.querySelectorAll('.field');
 
-canvas=document.getElementById("canvas");
-ctx=canvas.getContext('2d');
+let id; //id kliknutog polja
+let moves = 0; //broj poteza
+let clicked_fields = [0, 0, 0,  //detektujemokoje smo polje kliknuli 
+                      0, 0, 0, 
+                      0, 0, 0];
+let winner;
 
-var board=[[0, 0, 0, 0], [0, 0, 0, 2],
-           [0, 0, 0, 0], [0, 0, 0, 0]];
-var colors=["gray", "yellow", "orange", "red",
-            "bisque", "coral","blue", "palegreen", 
-            "pink", "purple", "linen", "brown"];
+fields.forEach(field => field.addEventListener('click', game));
 
-var score=0;
-
-//show board 4x4
-function  display(){
-    console.log(board);
-    for(var i=0; i<4; i++)
-        for(var j=0; j<4; j++){
-            if(board[i][j]!=0)
-                ctx.fillStyle=colors[Math.log2(board[i][j])];
-            else
-                ctx.fillStyle="azure";
-                
-            ctx.fillRect(i*100, j*100, 99, 99);
-            ctx.beginPath();
-            ctx.rect(i*100, j*100, 100, 100);
-            ctx.stroke();
-            if(board[i][j]!=0){
-                ctx.font="30px Arial";
-                ctx.fillStyle="black";
-                ctx.textAlign="center";
-                ctx.fillText(board[i][j], i*100+50, j*100+60);
-            }
-            
-        }
-}
-
-display();
-var disable=0;
-
-window.addEventListener('keydown', function (e) {
-    if( disable==1)
-	   return;
-	
-    key = e.keyCode;
-    if(key==37){
-    	e.preventDefault();
-    	left_key();
-    }
-    if(key==38){
-    	e.preventDefault();
-    	up_key();
-    }
-    if(key==39){
-    	e.preventDefault();
-    	right_key();
-    }
-    if(key==40){
-    	e.preventDefault();
-    	down_key();
-    }
-	
-	//if everything is full.
-	var count=0;
-	for(var i=0;i<4;i++)
-	for(var j=0;j<4;j++){
-		if( board[i][j]==0){
-            count++;
- 		}  
-	}
-	
-	if(count==0){
-        gameover();
-	 return;
-	}
-	else{
-	    var n=Math.floor(Math.random()*16);
-	    for(;n<32;n++){
-	        m=n%16;
-	        if(board[Math.floor(m/4)][m%4]==0){
-                board[Math.floor(m/4)][m%4]=2;
-	            break;
-	        }
-	    }
-    }
-	 display();
-});
-
-function left_key(){
-    for(var i=0; i<4; i++){
-        for(var j=0; j<4; j++){
-            if(board[i][j]){
-                var row=i;
-                while(row){
-                    if(!board[row-1][j]){
-                        board[row-1][j]=board[row][j];
-                        board[row][j]=0;
-                        row--;
-                    }
-                    else if(board[row-1][j]==board[row][j]){
-                        board[row-1][j]*=2;
-                        score+=board[row-1][j];
-                        board[row][j]=0;
-                        break;
-                    }
-                    else
-                        break;
-                }
-            }
-        }
-    }
-}
-
-function right_key() {
-  var i, j, row;
-  for(j = 0; j < 4; j++) {
-    for(i = 4 - 2; i >= 0; i--) {
-      if(board[i][j]) {
-        row = i;
-        while (row + 1 < 4) {
-          if (!board[row + 1][j]) {
-            board[row + 1][j] = board[row][j];
-            board[row][j] = 0;
-            row++;
-          } else if (board[row][j] == board[row + 1][j]) {
-            board[row + 1][j] *= 2;
-            score +=  board[row + 1][j];
-            board[row][j] = 0;
-            break;
-          } else {
-            break; 
-          }
-        }
-      }
-    }
+function start_game() {
+  let enter_name = document.getElementById("player");
+  if(enter_name.style.display === "none"){
+      enter_name.style.display = "none";
+  } 
+  else{
+  enter_name.style.display = "block"; //prikazi polje za unos imena
   }
 }
 
-function down_key() {
-  var i, j;
-  var coll;
-  for(i = 0; i < 4; i++) {
-    for(j = 4 - 2; j >= 0; j--) {
-      if(board[i][j]) {
-        coll = j;
-        while (coll + 1 < 4) {
-          if (!board[i][coll + 1]) {
-            board[i][coll + 1] = board[i][coll];
-            board[i][coll] = 0;
-            coll++;
-          } else if (board[i][coll] == board[i][coll + 1]) {
-            board[i][coll + 1] *= 2;
-            score +=  board[i][coll + 1];
-            board[i][coll] = 0;
-            break;
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
+// igrac je prvi na potezu
+function game() {
+  let ind_player = player_turn();
+  if(ind_player)
+      computer_turn();
 }
 
-function up_key() {
-  var i, j;
-  var coll;
-  for(i = 0; i < 4; i++) {
-    for(j = 1; j < 4; j++) {
-      if(board[i][j]) {
-        coll = j;
-        while (coll - 1 >= 0) {
-          if (!board[i][coll - 1]) {
-            board[i][coll - 1] = board[i][coll];
-            board[i][coll] = 0;
-            coll--;
-          } else if (board[i][coll] == board[i][coll - 1]) {
-            board[i][coll - 1] *= 2;
-            score +=   board[i][coll - 1];
-            board[i][coll] = 0;
-            break;
-          } else {
-            break; 
-          }
-        }
-      }
-    }
+function player_turn() {
+  // proveri da li ima pobednika
+  if(moves == 9) { //proveravamo da li je nereseno ili je ipak neko pobedio
+      check_board();
+      
+      if(winner == 0) 
+          alert("Nereseno")
+      else if (winner == 1)
+          alert("Pobedili ste");
+      else
+          alert("Nazalost, niste uspeli da pobedite");
+      return false;
   }
+  
+  if(clicked_fields[id] == 0) {
+      document.getElementById(id).innerHTML = "X";
+      clicked_fields[id] = 1;
+      
+      this.removeEventListener('click', game);
+      moves++;
+      
+      check_board();
+      if(winner == 1) {
+          alert("Pobedili ste");
+          exit();
+          return false;
+      }
+      else if(winner == 2) {
+          alert("Nazalost, niste uspeli da pobedite");
+          exit();
+          return false;
+      }
+  } else {
+      return false;
+  }
+  
+  return true;
 }
 
-function gameover(){
-    ctx.font = "40px Arial";
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.fillText("GAME OVER!!", 200,180); 
-    disable=1;
+// racunar je na potezu
+function computer_turn() {
+  // da li ima pobednika
+  if(moves == 9) {
+      check_board();
+      
+      if(winner == 0) 
+          alert("Nereseno")
+      else if (winner == 1)
+          alert("Pobedili ste")
+      else 
+          alert("Nazalost, niste uspeli da pobedite")
+      return;
+  }
+  
+  let num = Math.floor((Math.random() * 9));
+  while(clicked_fields[num] != 0) 
+      num = Math.floor((Math.random() * 9));
+  id = num;
+  document.getElementById(num).innerHTML = "O";
+  clicked_fields[num] = 2;
+  
+  
+  fields[num].removeEventListener('click', game);
+  moves++;
+  
+  check_board();
+  if(winner == 1) {
+      alert("Pobedili ste");
+      exit();
+  }
+  else if(winner == 2) {
+      alert("Nazalost, niste uspeli da pobedite");
+      exit();
+  }
+  
+  return;
+}
+
+// proveri stanje na tabeli
+function check_board() {
+    winner = 0;
+   if( (clicked_fields[0]==1 && clicked_fields[1]==1 && clicked_fields[2]==1) ||
+       (clicked_fields[3]==1 && clicked_fields[4]==1 && clicked_fields[5]==1) ||
+       (clicked_fields[6]==1 && clicked_fields[7]==1 && clicked_fields[8]==1) ||
+       (clicked_fields[0]==1 && clicked_fields[3]==1 && clicked_fields[6]==1) ||
+       (clicked_fields[1]==1 && clicked_fields[4]==1 && clicked_fields[7]==1) ||
+       (clicked_fields[2]==1 && clicked_fields[5]==1 && clicked_fields[8]==1) ||
+       (clicked_fields[0]==1 && clicked_fields[4]==1 && clicked_fields[8]==1) ||
+       (clicked_fields[2]==1 && clicked_fields[4]==1 && clicked_fields[6]==1)) {
+        winner = 1;
+    }
+    else if( (clicked_fields[0]==2 && clicked_fields[1]==2 && clicked_fields[2]==2) ||
+             (clicked_fields[3]==2 && clicked_fields[4]==2 && clicked_fields[5]==2) ||
+             (clicked_fields[6]==2 && clicked_fields[7]==2 && clicked_fields[8]==2) ||
+             (clicked_fields[0]==2 && clicked_fields[3]==2 && clicked_fields[6]==2) ||
+             (clicked_fields[1]==2 && clicked_fields[4]==2 && clicked_fields[7]==2) ||
+             (clicked_fields[2]==2 && clicked_fields[5]==2 && clicked_fields[8]==2) ||
+             (clicked_fields[0]==2 && clicked_fields[4]==2 && clicked_fields[8]==2) ||
+             (clicked_fields[2]==2 && clicked_fields[4]==2 && clicked_fields[6]==2)) {
+        winner = 2;
+ }
+    return;
+}
+
+function exit() {
+  fields.forEach(field => field.removeEventListener('click', game));
+}
+
+// id polja na koje je kliknuto
+function reply_click(clicked_id) {
+    id = clicked_id;
+}
+
+function restart_game(){
+  let ind_player=player_turn();
+  let moves = 0; 
+  let clicked_fields = [0, 0, 0, 
+                        0, 0, 0, 
+                        0, 0, 0];
+  let winner=0;
 }
